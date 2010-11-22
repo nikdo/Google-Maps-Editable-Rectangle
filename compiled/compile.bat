e3fe945a6cc18bddcfb64ceeccd02bde62636352
unknown <r.matej@.cz.gmc.net>
2010-11-23 00:46:24 +0100

    compilation

---
+++ compiled/compile.bat
@@ -0,0 +1,5 @@
+java -jar compiler.jar --js ../EditableRectangle.js --js ../RectangleHandles.js --js ../RectangleHandle.js --js_output_file EditableRectangle.js --compilation_level WHITESPACE_ONLY --formatting PRETTY_PRINT
+
+java -jar compiler.jar --js ../EditableRectangle.js --js ../RectangleHandles.js --js ../RectangleHandle.js --js_output_file EditableRectangle.min.js
+
+pause
